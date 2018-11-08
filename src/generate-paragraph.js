8db8b2bf483a991bfe9f36c333408b1dfb7358a9
Mark Aleksander Hil <mark@100shapes.com>
2018-11-08 16:34:03 +0000

    Create paragraph function

--- src/generate-paragraph.js
+++ src/generate-paragraph.js
@@ -2,16 +2,20 @@ import sketch from 'sketch'
 import {text} from './text'
 
 
-
-export default function() {
+function createParagraph() {
   var arr = text
-  var str = ""
+  var paragraph = []
   for (var i = 0; i < 4; i++) {
     var position = Math.floor(Math.random()*arr.length)
     var phrase = arr[position]
-    str += ". " + phrase
+    paragraph.push(phrase)
     arr.splice(position, 1)
   }
+  paragraph = paragraph.join(' ')
+  return paragraph
+}
+
+export default function() {
   var document = sketch.getSelectedDocument()
   var selectedLayers = document.selectedLayers
   var selectedCount = selectedLayers.length
@@ -19,27 +23,7 @@ export default function() {
     sketch.UI.message("Select a text layer.")
   } else {
     selectedLayers.forEach(function(layer, i) {
-      layer.text = str
+      layer.text = createParagraph()
     })
   }
-  // sketch.UI.message(str)
 }
-
-
-// export default function() {
-//   var arr = text
-//   var phrases = text.filter(phrase => phrase.length < 20)
-//   var phrase = phrases[Math.floor(Math.random() * phrases.length)]
-//   // sketch.UI.message(phrase)
-//   var document = sketch.getSelectedDocument()
-//   var selectedLayers = document.selectedLayers
-//   var selectedCount = selectedLayers.length
-//   console.log(selectedCount)
-//   if (selectedCount === 0) {
-//     sketch.UI.message("Select a text layer.")
-//   } else {
-//     selectedLayers.forEach(function(layer, i) {
-//       layer.text = phrase
-//     })
-//   }
-// }
